2ed6a0cf3f78328ca894f8ce8c643635080f0737
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-01-21 00:32:02 +0100

    html in js

--- ny-rainy-days-code/Javascript/all-jackets-api.js
+++ ny-rainy-days-code/Javascript/all-jackets-api.js
@@ -1,29 +1,34 @@
-import { apiUrl } from "./constants/api";
-import { consumer_key_secret } from "./constants/api";
+const apiUrl =
+  "https://emdevelopment.no/rainydays-product-lists/wp-json/wc/v3/products?consumer_key=ck_b86c196135fa5b41b37a700f48a7baef4a2c7cfe&consumer_secret=cs_5f525210a7deb0bc1e661148dadfc1bad899c4bf";
 
 const products = document.querySelector(".container");
 let productList = "";
 
 async function getProducts() {
-  //try{
-  const validation = {
-    method: "GET",
-    consumer_key_secret,
-    dataType: "json",
-  };
-
-  const getUrl = await fetch(apiUrl, validation);
+  const getUrl = await fetch(apiUrl);
 
   const apiResult = await getUrl.json();
 
-  const apiData = await apiResult.data;
+  const apiData = await apiResult;
 
   console.log(apiResult);
 
   apiData.forEach((product) => {
     console.log(product);
+
+    productList += `<div class="card-container">
+  <div class="card">
+  <a href="jacket-page.html">
+    <h3>${product.name}</h3>
+    <p>${product.short_description}</p>
+    <p class="price">${product.price}</p>
+  </a>
+  </div>
+  </div>
+`;
+
+    products.innerHTML = productList;
   });
-  //}
 }
 
 getProducts();
