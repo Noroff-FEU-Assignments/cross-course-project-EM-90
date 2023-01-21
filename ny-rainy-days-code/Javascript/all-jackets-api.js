56ce8ca6c2452d5b330ef53859460ae9fc26381c
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-01-21 21:02:23 +0100

    last commit

--- ny-rainy-days-code/Javascript/all-jackets-api.js
+++ ny-rainy-days-code/Javascript/all-jackets-api.js
@@ -2,6 +2,7 @@
 const apiUrl =
   "https://emdevelopment.no/rainydays-product-lists/wp-json/wc/v3/products?consumer_key=ck_b86c196135fa5b41b37a700f48a7baef4a2c7cfe&consumer_secret=cs_5f525210a7deb0bc1e661148dadfc1bad899c4bf";
 // const products = document.querySelector(".card-container");
+const products = document.querySelector(".card-container");
 let productList = "";
 
 const State = {
