05174268e7baeee14c2091572acc0a4d781c0bb4
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-01-21 22:21:24 +0100

    product-details commit

--- ny-rainy-days-code/Javascript/product-details.js
+++ ny-rainy-days-code/Javascript/product-details.js
@@ -1,33 +1,28 @@
-const details = document.querySelector(".jacket-grid");
-const string = document.location.search;
+/* METHOD */
+const GetProductIdFromParams = () => {
+  const string = document.location.search;
+  const params = new URLSearchParams(string);
+  return params.get("id");
+};
 
-const params = new URLSearchParams(string);
-
-const id = params.get("id");
-console.log(id);
-const apiUrl =
-  "https://emdevelopment.no/rainydays-product-lists/wp-json/wc/v3/products?consumer_key=ck_b86c196135fa5b41b37a700f48a7baef4a2c7cfe&consumer_secret=cs_5f525210a7deb0bc1e661148dadfc1bad899c4bf" +
-  id;
-
-async function getDetails() {
+async function GetProductInfo(productId) {
+  const URL = `https://emdevelopment.no/rainydays-product-lists/wp-json/wc/v3/products/${productId}?consumer_key=ck_b86c196135fa5b41b37a700f48a7baef4a2c7cfe&consumer_secret=cs_5f525210a7deb0bc1e661148dadfc1bad899c4bf`;
   try {
-    const result = await fetch(apiUrl);
-    const details = await result.json();
-    console.log(details);
+    const response = await fetch(URL);
+    const data = await result.json();
+    return data;
   } catch (error) {
-    console.log(error);
+    console.log("Something went wrong fetching ProductInfo", error);
   }
 }
 
-getDetails();
-
-function getJacket(jacket) {
-  console.log(jacket);
+function RenderJacketToTheDom(item) {
+  const details = document.querySelector(".jacket-grid");
   details.innerHTML = `<div class="product">
-    <div style="background-image: url("${jacket.images[0].src}")" class="product-image")></div>
+    <div style="background-image: url("${item.images[0].src}")" class="product-image")></div>
     <div class="product-info">
-     <h2>${jacket.name}</h2>
-     <h4>${jacket.description}</h4>
+     <h2>${item.name}</h2>
+     <h4>${item.description}</h4>
      <ul class="product-info-list">
        <li>Waterproof and wind resistant</li>
        <li>Good for medium and long trips</li>
@@ -35,8 +30,14 @@ function getJacket(jacket) {
        <li>Multiple pockets for more carrying space</li>
        <li>High quality zipper (waterproof YKK)</li>
      </ul>
-     <div class="jacket-price">${jacket.price}</div>
+     <div class="jacket-price">${item.price}</div>
 `;
 }
 
-getJacket(jacket);
+/* INITIALIZE */
+const productId = GetProductIdFromParams(); // Fetching "id" from the URL
+
+if (productId > 0) {
+  const product = GetProductInfo();
+  RenderJacketToTheDom(product);
+}
